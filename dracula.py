765b9eda86b9ad4d56f0a08a00c8c0a669675e67
Unknown <lucien.ledune@student.uclouvain.be>
2018-12-05 20:21:49 +0100

    Added text preprocessing, ponctuation removal, stemming, remove possible html tags
    Deleted table of content in txt file to avoid problems in chapter divsion

--- dracula.py
+++ dracula.py
@@ -24,8 +24,60 @@ for i in range(0, 27):
     expression = re.compile(r'%s.*?%s' % (Start,End), re.S)
     chapters.append(expression.search(data).group(0)) 
 
-Start = "CHAPTER I"
-End = "CHAPTER II"
-expression.search(data).group(0) 
+###########
 
+import numpy as np                                  #for large and multi-dimensional arrays
+import pandas as pd                                 #for data manipulation and analysis
+import nltk                                         #Natural language processing tool-kit
 
+from nltk.corpus import stopwords                   #Stopwords corpus
+from nltk.stem import PorterStemmer                 # Stemmer
+
+from sklearn.feature_extraction.text import CountVectorizer          #For Bag of words
+from sklearn.feature_extraction.text import TfidfVectorizer          #For TF-IDF
+from gensim.models import Word2Vec      
+nltk.download('stopwords')                             #For Word2Vec
+
+#stopwords
+stop = set(stopwords.words('english'))
+
+#basic text preprocessing
+for i in range(0, 27):
+    chapterUnique = chapters[i]
+    cleanr = re.compile('<.*?>')
+    chapterUnique = chapterUnique.lower()
+    chapterUnique = re.sub(cleanr, ' ', chapterUnique) #HTML tags
+    chapterUnique = re.sub(r'[?|!|\'|"|#]',r'',chapterUnique)
+    chapterUnique = re.sub(r'[.|,|)|(|\|/|_|-|;]',r' ',chapterUnique) #Punctuations
+    chapters[i] = chapterUnique
+
+temp = []
+wordsByChapter = []
+snow = nltk.stem.SnowballStemmer("english") #stemmer
+for i in range(0, 27):
+    chapterUnique = chapters[i]
+    words = [snow.stem(word) for word in chapterUnique.split() if word not in stop]
+    temp.append(words)
+    
+    
+#Convert back to string
+temp2 = []
+for i in range(0,27):
+    chapterStemmed = ''
+    chapterUniqueTemp = temp[i]
+    for word in chapterUniqueTemp:
+        chapterStemmed = chapterStemmed + ' ' + word
+    temp2.append(chapterStemmed)
+        
+chaptersStemmed = temp2
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
