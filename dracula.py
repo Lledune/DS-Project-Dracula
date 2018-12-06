7b6a81ae4225d0415b3f1cdd20db3c88dd7ab84a
Unknown <lucien.ledune@student.uclouvain.be>
2018-12-06 03:56:10 +0100

    Added tfidf cosine search engine

--- dracula.py
+++ dracula.py
@@ -83,14 +83,71 @@ for i in range(0,27):
 print(bowChapters[0])
 
 
-bowChapters[0].tobsr() 
 #♣OSCAR : Pour le bagofwords, si tu vois : (0,93) 23 par exemple, cela veut dire que me 93ème mot apparait 23 fois.
 #Tu peux voir de quel mot il s'agit en utilisant la liste temp comme ceci 
 temp[0][93] #word = "said"
-sp.shape(bowChapters[0])
-countV
-        
-        
-        
-        
-        
+
+########################################
+#This section aims to use the tfidf similarity cosine in order to make a simple search engine
+#The user fixes the query and the goal is to find the most similar chapter. 
+#For exemple the first chapter talks about diner and visiting, so our exemple query will return that the first chapter is the most 
+#similar to our query and therefore is the one we are looking for.
+#
+#To use this just change the query by a sentence of your choice (better if related to the book). 
+########################################
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+query = "I stopped at hotel royale, we had a good diner and i then visited a museum."
+cleanr = re.compile('<.*?>')
+query = query.lower()
+query = re.sub(cleanr, ' ', query) #HTML tags
+query = re.sub(r'[?|!|\'|"|#]',r'',query)
+query = re.sub(r'[.|,|)|(|\|/|_|-|;]',r' ',query) #Punctuations
+
+#Stem query
+snow = nltk.stem.SnowballStemmer("english") #stemmer
+query = [snow.stem(word) for word in query.split() if word not in stop]  #Stop is stopwords set
+
+#Back to string 
+queryStemmed = ''
+chapterUniqueTemp = temp[i]
+for word in query:
+    queryStemmed = queryStemmed + ' ' + word
+query = queryStemmed        
+
+#Now the query is preprocesed ...
+
+#Query added as first of the list, first of the list is the query for following code
+queryChaps = []
+queryChaps.append(query)
+queryChaps.extend(chaptersStemmed)
+
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix_train = tfidf_vectorizer.fit_transform(queryChaps)  #tfidf score with normalization
+
+scores = cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train) #here the first element of tfidf_matrix_train, since query is the first element of list
+#convert to list
+scores = scores.tolist()
+scores = scores[0]
+#change score of query since it is =1 and would prevent next line from working properly
+scores[0] = 0
+maxScore = max(scores)
+maxPos = [i for i, j in enumerate(scores) if j == maxScore] #!= 1 to avoid duplicates
+
+print("Tf-idf cosine scores : ", scores)
+print("The chapter you are looking for should be Chapter", maxPos)
+
+#############################################
+
+
+
+
+
+
+
+
+
+
+
