e84458dc9b67408e240d594a9551a34a8f3b6c9b
ozls <oscar.liessens@gmail.com>
2018-12-08 18:43:21 +0100

    Wordcloud fini

--- oscar.py
+++ oscar.py
@@ -40,7 +40,7 @@ import scipy as sp
 import sklearn as sk
 from sklearn.feature_extraction.text import CountVectorizer          #For Bag of words
 from sklearn.feature_extraction.text import TfidfVectorizer          #For TF-IDF
-from gensim.models import Word2Vec    
+from gensim.models import Word2Vec
   
 nltk.download('stopwords') #Stopwords list             
 
@@ -88,23 +88,69 @@ for i in range(0,27):
 print(bowChapters[2])
 temp[2][1]
 
-# Word cloud
+# Word cloud #########################
 
-from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+from PIL import Image
+from os import path
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS 
-from scipy.misc import imsave
+import os
+import random
 
-text = data
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-def generate_wordcloud(text): # optionally add: stopwords=STOPWORDS and change the arg below
-    wordcloud = WordCloud(relative_scaling = 1.0, stopwords = stop, width = 1920, height = 1080
-                          ).generate(text)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.axis('off')
-    plt.show()
-    imsave("wordcloud.png", wordcloud)
 
-generate_wordcloud(text)
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % 0
+
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = "D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula"
+
+# read the mask image taken from
+# http://www.stencilry.org/stencils/movies/star%20wars/storm-trooper.gif
+mask = np.array(Image.open(path.join(d, "drac.png")))
+
+# Texte
+text = open(path.join(d, 'dracula.txt')).read()
+
+# Stopwords
+stopwords = set(STOPWORDS)
+stopwords.add("said")
+stopwords.add("must")
+stopwords.add("will")
+stopwords.add("one")
+stopwords.add("n't")
+stopwords.add("may")
+
+tokens = np.array(nltk.word_tokenize(text))
+tagged = np.array(nltk.pos_tag(tokens))
+
+isnt_verb = np.empty(shape = (tokens.size), dtype = bool)
+for i in range(0, tokens.size-1):
+    if (tagged[i,1] != "VB" and tagged[i,1] != "VBD" and tagged[i,1] != "VBG"
+        and tagged[i,1] != "VBN" and tagged[i,1] != "VBP" and tagged[i,1] != "VBZ"):
+        isnt_verb[i] = True
+    else:
+        isnt_verb[i] = False
+        
+text_noverb = tokens[(isnt_verb)]
+text_niverbstr = " ".join(text_noverb)
+
+# Create wordcloud
+wc = WordCloud(background_color="white", max_words=1000, mask=mask, stopwords=stopwords, margin=10,
+               random_state=1).generate(text_niverbstr)
+default_colors = wc.to_array()
+plt.title("Custom colors")
+plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3),
+           interpolation="bilinear")
+wc.to_file("D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula/wordcloud.png")
+plt.axis("off")
+plt.show()
+
+
+
+
+
 
