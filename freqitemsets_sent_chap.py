81a19357bdd62cb61656a16b88100939f5836932
ozls <oscar.liessens@gmail.com>
2018-12-09 12:15:22 +0100

    freq item set terminé par phrase par chapitre
    Plus qu'à faire un graphique avec tout ça...

---
+++ freqitemsets_sent_chap.py
@@ -0,0 +1,196 @@
+from collections import Counter
+from itertools import combinations, chain
+import re
+import nltk
+from nltk.corpus import stopwords
+
+# Importing data by chapters
+
+filepath = "D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula/dracula.txt"
+
+startStops = [("CHAPTER I", "CHAPTER II"), ("CHAPTER II", "CHAPTER III"),
+              ("CHAPTER III", "CHAPTER IV"), ("CHAPTER IV", "CHAPTER V"),
+              ("CHAPTER V", "CHAPTER VI"), ("CHAPTER VI", "CHAPTER VII"),
+              ("CHAPTER VII", "CHAPTER VIII"), ("CHAPTER VIII", "CHAPTER IX"),
+              ("CHAPTER IX", "CHAPTER X"), ("CHAPTER X", "CHAPTER XI"),
+              ("CHAPTER XI", "CHAPTER XII"), ("CHAPTER XII", "CHAPTER XIII"),
+              ("CHAPTER XIII", "CHAPTER XIV"), ("CHAPTER XIV", "CHAPTER XV"),
+              ("CHAPTER XV", "CHAPTER XVI"), ("CHAPTER XVI", "CHAPTER XVII"),
+              ("CHAPTER XVII", "CHAPTER XVIII"), ("CHAPTER XVIII", "CHAPTER XIX"),
+              ("CHAPTER XIX", "CHAPTER XX"), ("CHAPTER XX", "CHAPTER XXI"),
+              ("CHAPTER XXI", "CHAPTER XXII"), ("CHAPTER XXII", "CHAPTER XXIII"),
+              ("CHAPTER XXIII", "CHAPTER XXIV"), ("CHAPTER XXIV", "CHAPTER XXV"),
+              ("CHAPTER XXV", "CHAPTER XXVI"),("CHAPTER XXVI", "CHAPTER XXVII"),
+              ("CHAPTER XXVII", "THE END")]
+
+chapters = []
+
+with open(filepath, 'r') as myfile:
+    data=myfile.read().replace('\n', ' ')
+
+for i in range(0, len(startStops)):
+    Start = startStops[i][0]
+    End = startStops[i][1]
+    expression = re.compile(r'%s.*?%s' % (Start,End), re.S)
+    chapters.append(expression.search(data).group(0))
+    
+# Creating sentences
+    
+sentencesc = []
+for i in range(0, len(chapters)):
+    sentencesc.append(nltk.tokenize.sent_tokenize(chapters[i]))
+
+# Stopwords
+    
+nltk.download('stopwords')
+stop = set(stopwords.words('english'))
+
+# Basic text preprocessing
+for k in range(0, len(sentencesc)):
+    for i in range(0, len(sentencesc[k])):
+        sentence = sentencesc[k][i]
+        cleanr = re.compile('<.*?>')
+        sentence = sentence.lower()
+        sentence = re.sub(cleanr, ' ', sentence)
+        sentence = re.sub(r'[?|!|\'|"|#]',r'',sentence)
+        sentence = re.sub(r'[.|,|)|(|\|/|_|-|;]',r' ',sentence)
+        sentencesc[k][i] = sentence
+
+wordssentc = []
+for k in range(0, len(sentencesc)):
+    wordssent = []
+    for i in range(0, len(sentencesc[k])):
+        sentence = sentencesc[k][i]
+        words = [word for word in sentence.split() if word not in stop]
+        wordssent.append(words)
+    wordssentc.append(wordssent)
+    
+    
+# Defining characters
+
+charlist = ["count", "dracula", "jonathan", "mina", "murray","arthur", "holwood",
+             "quincey", "morris", "renfield", "john", "seward", "abraham",
+             "helsing", "lucy", "westenra"]
+
+# Keeping characters
+
+charbysentc = []
+for k in range(0, len(wordssentc)):
+    charbysent = []
+    for i in range(0, len(wordssentc[k])):
+        charinsent = []
+        for j in range(0, len(wordssentc[k][i])):
+            if (wordssentc[k][i][j] in charlist):
+                charinsent.append(wordssentc[k][i][j])
+            if charinsent == []: continue
+            charbysent.append(charinsent)
+    charbysentc.append(charbysent)
+
+# Replacing the names of characters
+
+for k in range(0, len(charbysentc)):
+    for i in range(0, len(charbysentc[k])):
+        for j in range(0, len(charbysentc[k][i])):
+                    if charbysentc[k][i][j] == "count":  charbysentc[k][i][j] = "Count Dracula"
+                    elif charbysentc[k][i][j] == "dracula":  charbysentc[k][i][j] = "Count Dracula"
+                    elif charbysentc[k][i][j] == "jonathan":  charbysentc[k][i][j] = "Jonathan Harker"
+                    elif charbysentc[k][i][j] == "mina":  charbysentc[k][i][j] = "Mina Murray"
+                    elif charbysentc[k][i][j] == "murray":  charbysentc[k][i][j] = "Mina Murray"
+                    elif charbysentc[k][i][j] == "arthur":  charbysentc[k][i][j] = "Arthur Holwood"
+                    elif charbysentc[k][i][j] == "holwood":  charbysentc[k][i][j] = "Arthur Holwood"
+                    elif charbysentc[k][i][j] == "quincey":  charbysentc[k][i][j] = "Quincey Morris"
+                    elif charbysentc[k][i][j] == "morris":  charbysentc[k][i][j] = "Quincey Morris"
+                    elif charbysentc[k][i][j] == "renfield":  charbysentc[k][i][j] = "Renfield"
+                    elif charbysentc[k][i][j] == "john":  charbysentc[k][i][j] = "Dr. Seward"
+                    elif charbysentc[k][i][j] == "seward":  charbysentc[k][i][j] = "Dr. Seward"
+                    elif charbysentc[k][i][j] == "abraham":  charbysentc[k][i][j] = "Prof. Van Helsing"
+                    elif charbysentc[k][i][j] == "helsing":  charbysentc[k][i][j] = "Prof. Van Helsing"
+                    elif charbysentc[k][i][j] == "lucy":  charbysentc[k][i][j] = "Lucy Westenra"
+                    elif charbysentc[k][i][j] == "westenra":  charbysentc[k][i][j] = "Lucy Westenra"
+
+# Removing double character by sentence
+
+for k in range(0, len(charbysentc)):
+    for i in range(0, len(charbysentc[k])):
+        charbysentc[k][i] = list(set(charbysentc[k][i]))
+
+# Creating set
+
+charsetc = []
+for k in range(0, len(charbysentc)):  
+    charset = []
+    for i in range(0, len(charbysentc[k])):
+        sentence = []
+        for j in range(0, len(charbysentc[k][i])):
+            if (charbysentc[k][i][j] == "Count Dracula"): sentence.append("a")
+            elif (charbysentc[k][i][j] == "Jonathan Harker"): sentence.append("b")
+            elif (charbysentc[k][i][j] == "Mina Murray"):  sentence.append("c")
+            elif (charbysentc[k][i][j] == "Arthur Holwood"): sentence.append("d")
+            elif (charbysentc[k][i][j] == "Quincey Morris"): sentence.append("e")
+            elif (charbysentc[k][i][j] == "Renfield"):  sentence.append("f")
+            elif (charbysentc[k][i][j] == "Dr. Seward"): sentence.append("g")
+            elif (charbysentc[k][i][j] == "Prof. Van Helsing"): sentence.append("h")
+            elif (charbysentc[k][i][j] == "Lucy Westenra"): sentence.append("i")
+        charset.append(sentence)
+    charsetc.append(charset)
+
+# A priori algorithm
+
+def apriori(s, min_support, max_length):
+    c = Counter()
+
+    for t in s:
+        c.update(set(t))
+
+    frequent_sets = {1:[[set(i) for i in c if c[i]>=min_support],[c[i] for i in c if c[i]>=min_support]]}
+
+    current_level = 1
+
+    while len(frequent_sets[current_level][0]) >= 0 and (current_level < max_length):
+        frequent_sets[current_level+1] = [[],[]]
+        for i, j in combinations(frequent_sets[current_level][0],2):
+            new_set = i.copy()
+            new_set.update(j)
+            if len(new_set) == current_level + 1:
+                support = sum([new_set.issubset(set(t)) for t in s])
+                if support >= min_support:
+                    if new_set not in frequent_sets[current_level+1][0]:
+                        frequent_sets[current_level+1][0].append(new_set)
+                        frequent_sets[current_level+1][1].append(support)
+        current_level += 1
+    
+    return list(chain(*[[(x,y) for x,y in zip(a[0],a[1])] for a in frequent_sets.values()]))
+
+# Defining the application of a priori on the chapters, returning list of lists
+
+def apply_apriori (chap, min_support, max_length):
+    fisc = []
+    for k in range(0, len(chap)):
+        fis = apriori(chap[k], min_support, max_length)
+        fislist = []
+        for i in range(0, len(fis)):
+            fislist.append([list(fis[i][0]), fis[i][1]])
+        fisc.append(fislist)
+    for k in range(0, len(fisc)):
+        for i in range(0, len(fisc[k])):
+            for j in range(0, len(fisc[k][i][0])):
+                if (fisc[k][i][0][j] == "a"): fisc[k][i][0][j] = "Count Dracula"
+                elif (fisc[k][i][0][j] == "b"): fisc[k][i][0][j] = "Jonathan Harker"
+                elif (fisc[k][i][0][j] == "c"):  fisc[k][i][0][j] = "Mina Murray"
+                elif (fisc[k][i][0][j] == "d"): fisc[k][i][0][j] = "Arthur Holwood"
+                elif (fisc[k][i][0][j] == "e"): fisc[k][i][0][j] = "Quincey Morris"
+                elif (fisc[k][i][0][j] == "f"):  fisc[k][i][0][j] = "Renfield"
+                elif (fisc[k][i][0][j] == "g"): fisc[k][i][0][j] = "Dr. Seward"
+                elif (fisc[k][i][0][j] == "h"): fisc[k][i][0][j] = "Prof. Van Helsing"
+                elif (fisc[k][i][0][j] == "i"): fisc[k][i][0][j] = "Lucy Westenra"
+    return(fisc)
+
+# Applying apriori by chapter
+    
+fisc = apply_apriori(charsetc, 3, 10)
+            
+# Getting a list of frequent associations in all chapters
+            
+
+
+
--- freqitemsets_sentences.py
+++ freqitemsets_sentences.py
@@ -17,7 +17,7 @@ sentences = nltk.tokenize.sent_tokenize(data)
 
 # Stopwords
     
-nltk.download('stopwords')   
+nltk.download('stopwords')
 stop = set(stopwords.words('english'))
 
 # Basic text preprocessing
@@ -58,42 +58,47 @@ for i in range(0, len(wordssent)):
     
 for i in range(0, len(charbysent)):
     for j in range(0, len(charbysent[i])):
-        if (charbysent[i][j] == "count"):  charbysent[i][j] = "Count Dracula"
-        elif (charbysent[i][j] == "dracula"):  charbysent[i][j] = "Count Dracula"
-        elif (charbysent[i][j] == "jonathan"):  charbysent[i][j] = "Jonathan Harker"
-        elif (charbysent[i][j] == "mina"):  charbysent[i][j] = "Mina Harker"
-        elif (charbysent[i][j] == "arthur"):  charbysent[i][j] = "Arthur Holwood"
-        elif (charbysent[i][j] == "holwood"):  charbysent[i][j] = "Arthur Holwood"
-        elif (charbysent[i][j] == "quincey"):  charbysent[i][j] = "Quincey Morris"
-        elif (charbysent[i][j] == "morris"):  charbysent[i][j] = "Quincey Morris"
-        elif (charbysent[i][j] == "renfield"):  charbysent[i][j] = "Renfield"
-        elif (charbysent[i][j] == "john"):  charbysent[i][j] = "John Seward"
-        elif (charbysent[i][j] == "seward"):  charbysent[i][j] = "John Seward"
-        elif (charbysent[i][j] == "abraham"):  charbysent[i][j] = "Prof. Van Helsing"
-        elif (charbysent[i][j] == "helsing"):  charbysent[i][j] = "Prof. Van Helsing"
-        elif (charbysent[i][j] == "lucy"):  charbysent[i][j] = "Lucy Westenra"
-        elif (charbysent[i][j] == "westenra"):  charbysent[i][j] = "Lucy Westenra"
-        
+        if charbysent[i][j] == "count":  charbysent[i][j] = "Count Dracula"
+        elif charbysent[i][j] == "dracula":  charbysent[i][j] = "Count Dracula"
+        elif charbysent[i][j] == "jonathan":  charbysent[i][j] = "Jonathan Harker"
+        elif charbysent[i][j] == "mina":  charbysent[i][j] = "Mina Harker"
+        elif charbysent[i][j] == "arthur":  charbysent[i][j] = "Arthur Holwood"
+        elif charbysent[i][j] == "holwood":  charbysent[i][j] = "Arthur Holwood"
+        elif charbysent[i][j] == "quincey":  charbysent[i][j] = "Quincey Morris"
+        elif charbysent[i][j] == "morris":  charbysent[i][j] = "Quincey Morris"
+        elif charbysent[i][j] == "renfield":  charbysent[i][j] = "Renfield"
+        elif charbysent[i][j] == "john":  charbysent[i][j] = "John Seward"
+        elif charbysent[i][j] == "seward":  charbysent[i][j] = "John Seward"
+        elif charbysent[i][j] == "abraham":  charbysent[i][j] = "Prof. Van Helsing"
+        elif charbysent[i][j] == "helsing":  charbysent[i][j] = "Prof. Van Helsing"
+        elif charbysent[i][j] == "lucy":  charbysent[i][j] = "Lucy Westenra"
+        elif charbysent[i][j] == "westenra":  charbysent[i][j] = "Lucy Westenra"
+
 # Removing double character by sentence
 
 for i in range(0, len(charbysent)):
     charbysent[i] = list(set(charbysent[i]))
 
+# Creating set
+    
+charset = []
+for i in range(0, len(charbysent)):
+    sentence = []
+    for j in range(0, len(charbysent[i])):
+        if (charbysent[i][j] == "Count Dracula"): sentence.append("a")
+        elif (charbysent[i][j] == "Jonathan Harker"): sentence.append("b")
+        elif (charbysent[i][j] == "Mina Harker"):  sentence.append("c")
+        elif (charbysent[i][j] == "Arthur Holwood"): sentence.append("d")
+        elif (charbysent[i][j] == "Quincey Morris"): sentence.append("e")
+        elif (charbysent[i][j] == "Renfield"):  sentence.append("f")
+        elif (charbysent[i][j] == "John Seward"): sentence.append("g")
+        elif (charbysent[i][j] == "Prof. Van Helsing"): sentence.append("h")
+        elif (charbysent[i][j] == "Lucy Westenra"): sentence.append("i")
+    charset.append(sentence)
+
 # A priori algorithm
 
 def apriori(s, min_support, max_length):
-    """
-    :type s: list
-    :param s: list of lists, each sublist is a transaction, each element of the transaction is an item
-    :type min_support: int
-    :param min_support: minimal number of occurences for an itemset to be frequent
-    :type max_length: int
-    :param max_length: maximal length of searched itemsets
-    :rtype: list
-    :return: list of tuples. Each tuple contains 2 elements: the first element is a set representing a frequent
-    itemset, the second element is its support in s.
-    """
-
     c = Counter()
 
     for t in s:
@@ -120,4 +125,21 @@ def apriori(s, min_support, max_length):
 
 # Applying a priori
     
-fis = apriori(charbysent, 2, 10)
+fis = apriori(charset, 4, 10)
+fislist = []
+for i in range(0, len(fis)):
+    fislist.append([list(fis[i][0]), fis[i][1]])
+
+# Getting FIS with character names
+
+for i in range(0, len(fislist)):
+    for j in range(0, len(fislist[i][0])):
+        if (fislist[i][0][j] == "a"): fislist[i][0][j] = "Count Dracula"
+        elif (fislist[i][0][j] == "b"): fislist[i][0][j] = "Jonathan Harker"
+        elif (fislist[i][0][j] == "c"):  fislist[i][0][j] = "Mina Harker"
+        elif (fislist[i][0][j] == "d"): fislist[i][0][j] = "Arthur Holwood"
+        elif (fislist[i][0][j] == "e"): fislist[i][0][j] = "Quincey Morris"
+        elif (fislist[i][0][j] == "f"):  fislist[i][0][j] = "Renfield"
+        elif (fislist[i][0][j] == "g"): fislist[i][0][j] = "John Seward"
+        elif (fislist[i][0][j] == "h"): fislist[i][0][j] = "Prof. Van Helsing"
+        elif (fislist[i][0][j] == "i"): fislist[i][0][j] = "Lucy Westenra"
