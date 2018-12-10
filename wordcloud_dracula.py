724e6b3cf42259331dec325172968ac9c24cb938
ozls <oscar.liessens@gmail.com>
2018-12-10 11:52:56 +0100

    It's fucking done bitch
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- wordcloud_dracula.py
+++ wordcloud_dracula.py
@@ -1,24 +1,17 @@
 import numpy as np
 from PIL import Image
-from os import path
 import matplotlib.pyplot as plt
 import nltk
 from wordcloud import WordCloud, STOPWORDS
 
-def grey_color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
-    return "hsl(0, 0%%, %d%%)" % 0
+#################### CHANGE THE PATH BELOW TO MAKE THE CODE RUN ############################
+###### The path mentioned here must be a folder which contains all complementary files #####
+filepath = "D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula/"
 
-
-# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-d = "D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula"
-
-# read the mask image taken from
-# http://www.stencilry.org/stencils/movies/star%20wars/storm-trooper.gif
-mask = np.array(Image.open(path.join(d, "drac.png")))
+mask = np.array(Image.open(filepath+"Images/drac.png"))
 
 # Texte
-text = open(path.join(d, 'dracula.txt')).read()
+text = open(filepath+"dracula.txt").read()
 
 # Stopwords
 stopwords = set(STOPWORDS)
@@ -43,6 +36,10 @@ for i in range(0, tokens.size-1):
 text_noverb = tokens[(isnt_verb)]
 text_niverbstr = " ".join(text_noverb)
 
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % 0
+
 # Create wordcloud
 wc = WordCloud(background_color="white", max_words=1000, mask=mask, stopwords=stopwords, margin=10,
                random_state=1).generate(text_niverbstr)
@@ -50,7 +47,7 @@ default_colors = wc.to_array()
 plt.title("Custom colors")
 plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3),
            interpolation="bilinear")
-wc.to_file("D:/Google Drive/DATS2MS/LINMA2472 Algorithms in data science/Project/Dracula/wordcloud.png")
+wc.to_file(filepath+"Images/wordcloud.png")
 plt.axis("off")
 plt.show()
 
